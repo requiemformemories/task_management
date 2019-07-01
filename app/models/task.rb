81a4eb5a5f6db8125a553aab30e51b094b6b595c
fumitsuki <fumitsuki@ntuosc.org>
2019-07-01 22:37:31 +0800

    update basic CRUD of tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,19 @@
 class Task < ApplicationRecord
-  validates_presence_of :taskid, :status
+  before_create :generate_id, :fill_status
+
   validates_uniqueness_of :taskid
-  belongs_to :category
+  belongs_to :category, optional: true
   has_many :user_taskships
   has_many :users, :through => :user_taskships
   has_many :tag_taskships
   has_many :tags, :through => :tag_taskships
+
+  private
+  def generate_id
+    self.taskid = SecureRandom.uuid
+  end
+
+  def fill_status
+    self.status = 0
+  end
 end
