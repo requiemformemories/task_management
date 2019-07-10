6856f0fb46d8f4767762bed13b3348e15240ca2e
fumitsuki <fumitsuki@ntuosc.org>
2019-07-10 13:22:35 +0000

    view fix

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,34 +1,9 @@
 module TasksHelper
-  def status_text(status)
-    case status
-      when "pending"
-        return t("task.pending")
-      when "in_progress"
-        return t("task.in_progress")
-      when "finished"
-        return t("task.finished")
-      else
-        return ""
-    end
-  end
-  
-  def priority_text(priority)
-    case priority
-      when 0
-        return t("task.priority_0")
-      when 1
-        return t("task.priority_1")
-      when 2
-        return t("task.priority_2")
-      else
-        return ""
-    end    
-  end
   
   def status_options
-     id = Task.aasm.states.map &:name
-     name = id.map {|e| I18n.t("task."+e.to_s) }
-     return name.zip(id).to_h
+    id = Task.aasm.states.map &:name
+    name = id.map {|e| I18n.t("task."+e.to_s) }
+    return name.zip(id).to_h
   end  
   
   def priority_options
